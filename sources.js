af05892380541ad4d5318cfa5c9ca57b1f6ed8d8
viendinh <vienaone1475@gmail.com>
2026-07-26 07:13:00 +0700

    Add SWD392 question source

--- sources.js
+++ sources.js
@@ -1,7 +1,4 @@
-// Danh sách các nguồn câu hỏi.
-// Khi thêm môn mới: tạo file JS theo mẫu source-template.js rồi thêm 1 dòng vào đây.
 window.quizSourceFiles = [
-  "questions.js"
-  // Ví dụ: "swd392.js",
-  // Ví dụ: "prf192.js"
+  "questions.js",
+  "swd392.js"
 ];
